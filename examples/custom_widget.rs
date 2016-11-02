d42dfaa71081ef8d4fbbd4f378b23919594a5a62
Florian Dehau <florian.dehau@telecomnancy.net>
2016-11-02 19:17:18 +0100

    Add example for implementing a custom widget

---
+++ examples/custom_widget.rs
@@ -0,0 +1,41 @@
+extern crate tui;
+
+use tui::Terminal;
+use tui::widgets::Widget;
+use tui::buffer::Buffer;
+use tui::layout::Rect;
+use tui::style::Color;
+
+struct Label<'a> {
+    text: &'a str,
+}
+
+impl<'a> Default for Label<'a> {
+    fn default() -> Label<'a> {
+        Label { text: "" }
+    }
+}
+
+impl<'a> Widget for Label<'a> {
+    fn draw(&self, area: &Rect, buf: &mut Buffer) {
+        buf.set_string(area.left(),
+                       area.top(),
+                       self.text,
+                       Color::Reset,
+                       Color::Reset);
+    }
+}
+
+impl<'a> Label<'a> {
+    fn text(&mut self, text: &'a str) -> &mut Label<'a> {
+        self.text = text;
+        self
+    }
+}
+
+fn main() {
+    let mut terminal = Terminal::new().unwrap();
+    terminal.clear();
+    Label::default().text("Test").render(&Terminal::size().unwrap(), &mut terminal);
+    terminal.finish();
+}
