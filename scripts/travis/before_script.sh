f3626782340188c282b9d2e89c6f20cef601da26
Florian Dehau <work@fdehau.com>
2017-10-30 22:39:10 +0100

    [travis] Fix shared libs loading

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -4,5 +4,6 @@ set -eu
 
 export PATH="$PATH:$HOME/.cargo/bin"
 if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  export LD_LIBRARY_PATH=$(rustc +nightly --print sysroot)/lib:$LD_LIBRARY_PATH
   make install-tools
 fi
