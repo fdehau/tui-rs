e4af718053c7645287cacc30317eeb5fe0daad1c
Florian Dehau <work@fdehau.com>
2017-10-30 22:58:34 +0100

    [clippy] Fix warnings

--- src/symbols.rs
+++ src/symbols.rs
@@ -1,36 +1,36 @@
 pub mod block {
-    pub const FULL: &'static str = "█";
-    pub const SEVEN_EIGHTHS: &'static str = "▉";
-    pub const THREE_QUATERS: &'static str = "▊";
-    pub const FIVE_EIGHTHS: &'static str = "▋";
-    pub const HALF: &'static str = "▌";
-    pub const THREE_EIGHTHS: &'static str = "▍";
-    pub const ONE_QUATER: &'static str = "▎";
-    pub const ONE_EIGHTH: &'static str = "▏";
+    pub const FULL: &str = "█";
+    pub const SEVEN_EIGHTHS: &str = "▉";
+    pub const THREE_QUATERS: &str = "▊";
+    pub const FIVE_EIGHTHS: &str = "▋";
+    pub const HALF: &str = "▌";
+    pub const THREE_EIGHTHS: &str = "▍";
+    pub const ONE_QUATER: &str = "▎";
+    pub const ONE_EIGHTH: &str = "▏";
 }
 
 pub mod bar {
-    pub const FULL: &'static str = "█";
-    pub const SEVEN_EIGHTHS: &'static str = "▇";
-    pub const THREE_QUATERS: &'static str = "▆";
-    pub const FIVE_EIGHTHS: &'static str = "▅";
-    pub const HALF: &'static str = "▄";
-    pub const THREE_EIGHTHS: &'static str = "▃";
-    pub const ONE_QUATER: &'static str = "▂";
-    pub const ONE_EIGHTH: &'static str = "▁";
+    pub const FULL: &str = "█";
+    pub const SEVEN_EIGHTHS: &str = "▇";
+    pub const THREE_QUATERS: &str = "▆";
+    pub const FIVE_EIGHTHS: &str = "▅";
+    pub const HALF: &str = "▄";
+    pub const THREE_EIGHTHS: &str = "▃";
+    pub const ONE_QUATER: &str = "▂";
+    pub const ONE_EIGHTH: &str = "▁";
 }
 
 pub mod line {
-    pub const TOP_RIGHT: &'static str = "┐";
-    pub const VERTICAL: &'static str = "│";
-    pub const HORIZONTAL: &'static str = "─";
-    pub const TOP_LEFT: &'static str = "┌";
-    pub const BOTTOM_RIGHT: &'static str = "┘";
-    pub const BOTTOM_LEFT: &'static str = "└";
-    pub const VERTICAL_LEFT: &'static str = "┤";
-    pub const VERTICAL_RIGHT: &'static str = "├";
-    pub const HORIZONTAL_DOWN: &'static str = "┬";
-    pub const HORIZONTAL_UP: &'static str = "┴";
+    pub const TOP_RIGHT: &str = "┐";
+    pub const VERTICAL: &str = "│";
+    pub const HORIZONTAL: &str = "─";
+    pub const TOP_LEFT: &str = "┌";
+    pub const BOTTOM_RIGHT: &str = "┘";
+    pub const BOTTOM_LEFT: &str = "└";
+    pub const VERTICAL_LEFT: &str = "┤";
+    pub const VERTICAL_RIGHT: &str = "├";
+    pub const HORIZONTAL_DOWN: &str = "┬";
+    pub const HORIZONTAL_UP: &str = "┴";
 }
 
-pub const DOT: &'static str = "•";
+pub const DOT: &str = "•";
