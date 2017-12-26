7f8bcc50bf678491e3ed2315a234ef087011ac78
Florian Dehau <work@fdehau.com>
2017-12-26 21:29:51 +0100

    [src][chart] fmt

--- src/widgets/chart.rs
+++ src/widgets/chart.rs
@@ -274,10 +274,10 @@ where
         }
 
         if let Some(y_labels) = self.y_axis.labels {
-            let mut max_width = y_labels.iter().fold(
-                0,
-                |acc, l| max(l.as_ref().width(), acc),
-            ) as u16;
+            let mut max_width = y_labels
+                .iter()
+                .fold(0, |acc, l| max(l.as_ref().width(), acc))
+                as u16;
             if let Some(x_labels) = self.x_axis.labels {
                 if x_labels.len() > 0 {
                     max_width = max(max_width, x_labels[0].as_ref().width() as u16);
