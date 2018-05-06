7e454866cb15e384366de50c50995ea89a751b4c
Florian Dehau <work@fdehau.com>
2018-05-06 12:50:03 +0200

    Simplify travis configuration

--- scripts/travis/before_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-export PATH="$PATH:$HOME/.cargo/bin"
-if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
-  export LD_LIBRARY_PATH="$(rustc +nightly --print sysroot)/lib:${LD_LIBRARY_PATH:-""}"
-  make install-tools
-fi
