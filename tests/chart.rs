abd552fde65d45ca78f3cd21582838b15348ef31
Florian Dehau <work@fdehau.com>
2019-07-16 08:03:02 +0200

    Run cargo fmt

--- tests/chart.rs
+++ tests/chart.rs
@@ -11,27 +11,23 @@ fn zero_axes_ok() {
 
     terminal
         .draw(|mut f| {
-        Chart::default()
-            .block(Block::default().title("Plot").borders(Borders::ALL))
-            .x_axis(
-                Axis::default()
-                    .bounds([ 0.0, 0.0, ])
-                    .labels(&["0.0", "1.0"])
-            )
-            .y_axis(
-                Axis::default()
-                    .bounds([ 0.0, 1.0, ])
-                    .labels(&["0.0", "1.0"])
-            )
-            .datasets(&[Dataset::default()
-                .marker(Marker::Braille)
-                .style(Style::default().fg(Color::Magenta))
-                .data(&[(0.0, 0.0)])])
-            .render(&mut f, Rect {
-                x: 0,
-                y: 0,
-                width: 100,
-                height: 100,
-            });
-        }).unwrap();
+            Chart::default()
+                .block(Block::default().title("Plot").borders(Borders::ALL))
+                .x_axis(Axis::default().bounds([0.0, 0.0]).labels(&["0.0", "1.0"]))
+                .y_axis(Axis::default().bounds([0.0, 1.0]).labels(&["0.0", "1.0"]))
+                .datasets(&[Dataset::default()
+                    .marker(Marker::Braille)
+                    .style(Style::default().fg(Color::Magenta))
+                    .data(&[(0.0, 0.0)])])
+                .render(
+                    &mut f,
+                    Rect {
+                        x: 0,
+                        y: 0,
+                        width: 100,
+                        height: 100,
+                    },
+                );
+        })
+        .unwrap();
 }
