1e56fff8716f0b065a80f14dde76528abe29673c
DarrienG <darrienglasser@outlook.com>
2020-05-09 15:25:48 -0400

    Add test to ensure codepath doesn't break

--- tests/chart.rs
+++ tests/chart.rs
@@ -35,3 +35,40 @@ fn zero_axes_ok() {
         })
         .unwrap();
 }
+
+#[test]
+fn handles_overflow() {
+    let backend = TestBackend::new(80, 30);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    terminal
+        .draw(|mut f| {
+            let datasets = [Dataset::default()
+                .marker(symbols::Marker::Braille)
+                .style(Style::default().fg(Color::Magenta))
+                .data(&[
+                    (1588298471.0, 1.0),
+                    (1588298473.0, 0.0),
+                    (1588298496.0, 1.0),
+                ])];
+            let chart = Chart::default()
+                .block(Block::default().title("Plot").borders(Borders::ALL))
+                .x_axis(
+                    Axis::default()
+                        .bounds([1588298471.0, 1588992600.0])
+                        .labels(&["1588298471.0", "1588992600.0"]),
+                )
+                .y_axis(Axis::default().bounds([0.0, 1.0]).labels(&["0.0", "1.0"]))
+                .datasets(&datasets);
+            f.render_widget(
+                chart,
+                Rect {
+                    x: 0,
+                    y: 0,
+                    width: 80,
+                    height: 30,
+                },
+            );
+        })
+        .unwrap();
+}
