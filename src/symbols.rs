c35a1dd79fd59e37999a1a64b5faa7018f95e9dd
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-09-20 20:57:35 +0800

    feat(widgets/canvas): added type Block in canvas markers (#350)
    This allows for clearer colors than using Dot, especially when
    decreasing the size of the terminal font in order to increase the
    resolution of the canvas

--- src/symbols.rs
+++ src/symbols.rs
@@ -224,8 +224,10 @@ pub mod braille {
 /// Marker to use when plotting data points
 #[derive(Debug, Clone, Copy)]
 pub enum Marker {
-    /// One point per cell
+    /// One point per cell in shape of dot
     Dot,
+    /// One point per cell in shape of a block
+    Block,
     /// Up to 8 points per cell
     Braille,
 }
