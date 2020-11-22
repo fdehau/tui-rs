4ce8ed0c86388bb9323c5271c23e188da006eb90
DashEightMate <loganserino@gmail.com>
2020-11-21 19:51:33 -0500

    gauge tests now check color

--- tests/widgets_gauge.rs
+++ tests/widgets_gauge.rs
@@ -1,10 +1,4 @@
-use tui::{
-    backend::TestBackend,
-    buffer::Buffer,
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Gauge},
-    Terminal,
-};
+use tui::{style::Color, Terminal, backend::TestBackend, buffer::Buffer, layout::{Constraint, Direction, Layout}, style::Style, widgets::{Block, Borders, Gauge}};
 
 #[test]
 fn widgets_gauge_renders() {
@@ -20,26 +14,43 @@ fn widgets_gauge_renders() {
 
             let gauge = Gauge::default()
                 .block(Block::default().title("Percentage").borders(Borders::ALL))
+                .gauge_style(Style::default().bg(Color::Blue).fg(Color::Red))
                 .percent(43);
             f.render_widget(gauge, chunks[0]);
             let gauge = Gauge::default()
                 .block(Block::default().title("Ratio").borders(Borders::ALL))
-                .ratio(0.211_313_934_313_1);
+                .gauge_style(Style::default().bg(Color::Blue).fg(Color::Red))
+                .ratio(0.511_313_934_313_1);
             f.render_widget(gauge, chunks[1]);
         })
         .unwrap();
-    let expected = Buffer::with_lines(vec![
+    let mut expected = Buffer::with_lines(vec![
         "                                        ",
         "                                        ",
         "  ┌Percentage────────────────────────┐  ",
         "  │              ▋43%                │  ",
         "  └──────────────────────────────────┘  ",
         "  ┌Ratio─────────────────────────────┐  ",
-        "  │       ▏       21%                │  ",
+        "  │               51%                │  ",
         "  └──────────────────────────────────┘  ",
         "                                        ",
         "                                        ",
     ]);
+
+    for i in 3..17 {
+        expected.get_mut(i, 3).set_bg(Color::Red).set_fg(Color::Blue);
+    }
+    for i in 17..37 {
+        expected.get_mut(i, 3).set_bg(Color::Blue).set_fg(Color::Red);
+    }
+
+    for i in 3..20 {
+        expected.get_mut(i, 6).set_bg(Color::Red).set_fg(Color::Blue);
+    }
+    for i in 20..37 {
+        expected.get_mut(i, 6).set_bg(Color::Blue).set_fg(Color::Red);
+    }
+    
     terminal.backend().assert_buffer(&expected);
 }
 
