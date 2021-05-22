6ea9be30cbeb44f1aa371739a7b54e5dac336403
C-Jameson <booksncode@gmail.com>
2021-05-22 10:26:06 -0400

    moved tests around
    split tests up to make it easier to see why it's failing.

--- tests/border_macro.rs
+++ tests/border_macro.rs
@@ -10,12 +10,18 @@ use tui::{
 };
 
 #[test]
-fn border_macro_test() {
+fn border_empty_test() {
     let empty = Borders::empty();
-    let all = Borders::all();
-    let left_right = Borders::from_bits(Borders::LEFT.bits() | Borders::RIGHT.bits());
     assert_eq!(empty, border!());
+}
+#[test]
+fn border_all_test() {
+    let all = Borders::all();
     assert_eq!(all, border!(ALL));
     assert_eq!(all, border!(TOP, BOTTOM, LEFT, RIGHT));
+}
+#[test]
+fn border_left_right_test() {
+    let left_right = Borders::from_bits(Borders::LEFT.bits() | Borders::RIGHT.bits());
     assert_eq!(left_right, Some(border!(RIGHT, LEFT)));
 }
